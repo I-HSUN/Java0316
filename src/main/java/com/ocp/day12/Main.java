b0030b840b3fabcf3940aba5fb7254332d828932
MB-study <MB-study@192.168.1.118>
2021-04-15 19:09:17 +0800

    Java

---
+++ src/main/java/com/ocp/day12/Main.java
@@ -0,0 +1,11 @@
+package com.ocp.day12;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.shout();
+        Tiger tiger = new Tiger();
+        tiger.shout();
+    }
+    
+}
---
+++ src/main/java/com/ocp/day12/Tiger.java
@@ -0,0 +1,8 @@
+package com.ocp.day12;
+
+public class Tiger extends Cat{
+    @Override
+    public void shout() {
+        System.out.println("吼~~~");
+    }
+}
