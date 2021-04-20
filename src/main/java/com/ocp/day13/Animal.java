b8527db60d6d10808ecb0b61b8686e8aa4e915ae
MB-study <MB-study@192.168.1.118>
2021-04-20 19:07:23 +0800

    Java

---
+++ src/main/java/com/ocp/day13/Animal.java
@@ -0,0 +1,7 @@
+package com.ocp.day13;
+
+public interface Animal {
+    int legs = 4;//public static final int legs = 4;
+    void eat();//public abstract void eat();
+    void move();//public abstract void move();
+}
