f70a8883e87c73583c9d7f4b87959dd1e0618401
MB-study <MB-study@192.168.1.118>
2021-04-20 21:23:23 +0800

    Java

---
+++ src/main/java/com/ocp/day15/Tank.java
@@ -0,0 +1,16 @@
+package com.ocp.day15;
+
+public class Tank extends Car implements Weapon{
+
+    @Override
+    public void shoot() {
+        System.out.println("穿甲彈");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("用履帶走");
+    }
+
+   
+}
---
+++ src/main/java/com/ocp/day15/Truck.java
@@ -0,0 +1,11 @@
+package com.ocp.day15;
+
+public class Truck extends Car{
+
+    @Override
+    public void move() {
+         System.out.println("用輪子走");
+    }
+
+      
+}
